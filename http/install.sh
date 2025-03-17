ecb6978c6ca0c92ee45ecc2250c66e2ea45d866c
andre-ortega <ortega.andre.j@gmail.com>
2025-03-16 23:27:33 -0700

    add archinstall wrapper

--- http/install.sh
+++ http/install.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+set -eou pipefail
+
+curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/user_configuration.json
+curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/user_credentials.json
 archinstall --config user_configuration.json --creds user_credentials.json --silent
+
+curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/autologin.conf
+mkdir /mnt/archinstall/etc/systemd/system/getty@tty1.service.d/
+mv autologin.conf /mnt/archinstall/etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo "roe ALL=(ALL) NOPASSWD: ALL" > /mnt/archinstall/etc/sudoers.d/00_roe
+
+curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/configure.sh
+mv configure.sh /mnt/archinstall/home/roe/
+cp /mnt/archinstall/home/roe/.bash_profile /mnt/archinstall/home/roe/.bash_profile.bak
+echo "sudo chown roe:roe ./configure.sh" >> /mnt/archinstall/home/roe/.bash_profile
+echo "chmod +x /home/roe/configure.sh" >> /mnt/archinstall/home/roe/.bash_profile
+echo "/home/roe/configure.sh" >> /mnt/archinstall/home/roe/.bash_profile
+shutdown now -r
