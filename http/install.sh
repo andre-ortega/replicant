71407d9685c2b73f22399c1140184c694caef7ba
andre-ortega <ortega.andre.j@gmail.com>
2025-10-19 12:35:49 -0700

    archinstall update mounting quirk

--- http/install.sh
+++ http/install.sh
@@ -19,19 +19,21 @@ set -eou pipefail
 # Keep your hands off the system during execution
 # You will know the system is ready when you see your word again
 
+# There will need to be some cheching to see if this is a live environment
+
 curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/user_configuration.json
 curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/user_credentials.json
 archinstall --config user_configuration.json --creds user_credentials.json --silent
 
 curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/autologin.conf
-mkdir /mnt/archinstall/etc/systemd/system/getty@tty1.service.d/
-mv autologin.conf /mnt/archinstall/etc/systemd/system/getty@tty1.service.d/autologin.conf
-echo "roe ALL=(ALL) NOPASSWD: ALL" > /mnt/archinstall/etc/sudoers.d/00_roe
+mkdir /mnt/etc/systemd/system/getty@tty1.service.d/
+mv autologin.conf /mnt/etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo "roe ALL=(ALL) NOPASSWD: ALL" > /mnt/etc/sudoers.d/00_roe
 
 curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/configure.sh
-mv configure.sh /mnt/archinstall/home/roe/
-cp /mnt/archinstall/home/roe/.bash_profile /mnt/archinstall/home/roe/.bash_profile.bak
-echo "sudo chown roe:roe ./configure.sh" >> /mnt/archinstall/home/roe/.bash_profile
-echo "chmod +x /home/roe/configure.sh" >> /mnt/archinstall/home/roe/.bash_profile
-echo "/home/roe/configure.sh" >> /mnt/archinstall/home/roe/.bash_profile
+mv configure.sh /mnt/home/roe/
+cp /mnt/home/roe/.bash_profile /mnt/home/roe/.bash_profile.bak
+echo "sudo chown roe:roe ./configure.sh" >> /mnt/home/roe/.bash_profile
+echo "chmod +x /home/roe/configure.sh" >> /mnt/home/roe/.bash_profile
+echo "/home/roe/configure.sh" >> /mnt/home/roe/.bash_profile
 shutdown -r now
