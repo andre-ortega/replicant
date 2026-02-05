be9acf667a8115bbe92b5ca6c989068e56484364
andre-ortega <ortega.andre.j@gmail.com>
2026-02-05 00:24:53 -0800

    back in the seat

--- http/install.sh
+++ http/install.sh
@@ -19,10 +19,12 @@ set -eou pipefail
 # Keep your hands off the system during execution
 # You will know the system is ready when you see your word again
 
-# There will need to be some cheching to see if this is a live environment
+# There will need to be some checking to see if this is a live environment
 
+# --SIM
 curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/user_configuration.json
 curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/user_credentials.json
+
 archinstall --config user_configuration.json --creds user_credentials.json --silent
 
 curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/autologin.conf
@@ -31,9 +33,10 @@ mv autologin.conf /mnt/etc/systemd/system/getty@tty1.service.d/autologin.conf
 echo "roe ALL=(ALL) NOPASSWD: ALL" > /mnt/etc/sudoers.d/00_roe
 
 curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/configure.sh
-mv configure.sh /mnt/home/roe/
+curl -O $PACKER_HTTP_IP:$PACKER_HTTP_PORT/replicate.sh
+mv configure.sh replicate.sh /mnt/home/roe/
 cp /mnt/home/roe/.bash_profile /mnt/home/roe/.bash_profile.bak
-echo "sudo chown roe:roe ./configure.sh" >> /mnt/home/roe/.bash_profile
-echo "chmod +x /home/roe/configure.sh" >> /mnt/home/roe/.bash_profile
+echo "sudo chown roe:roe ./configure.sh replicate.sh" >> /mnt/home/roe/.bash_profile
+echo "chmod +x /home/roe/configure.sh replicate.sh" >> /mnt/home/roe/.bash_profile
 echo "/home/roe/configure.sh" >> /mnt/home/roe/.bash_profile
 shutdown -r now
